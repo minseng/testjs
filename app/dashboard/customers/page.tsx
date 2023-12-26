fac0b268fcd88454791c8ba50f9258df19bf2c1a
조민성 <jms6894@naver.com>
2023-12-26 16:46:01 +0900

    2차 테스트

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import CustomersTable from '@/app/ui/dashboard/customers-table';
+
+
+export default function Page(){
+    return(
+        <>
+        <p>Customers Page</p>
+        <CustomersTable/>
+        </>
+    );
+}
---
+++ app/dashboard/members/page.tsx
@@ -0,0 +1,7 @@
+export default function Page(){
+    return(
+        <>
+        <p>members Page</p>
+        </>
+    );
+}
